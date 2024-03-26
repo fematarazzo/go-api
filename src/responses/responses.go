4d3b40b95b666eca88e8d5c4bcddfd8f206b229b
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-26 17:25:51 -0300

    add: responses package with JSON and Error function for handling errors

---
+++ src/responses/responses.go
@@ -0,0 +1,26 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if data != nil {
+		if error := json.NewEncoder(w).Encode(data); error != nil {
+			log.Fatal(error)
+		}
+	}
+}
+
+func Error(w http.ResponseWriter, statusCode int, error error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: error.Error(),
+	})
+}
