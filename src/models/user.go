0815f4c688408d62a23c65ebc720755a45719138
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-26 15:20:35 -0300

    add: model package with User struct

---
+++ src/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nickname  string    `json:"nickname,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
