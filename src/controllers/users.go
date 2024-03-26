2a85bf9e4be3426a819f0b57197cb56726775120
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-26 17:24:11 -0300

    fix: typo

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,9 +1,44 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("User created\n"))
+	bodyRequest, error := io.ReadAll(r.Body)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	var user models.User
+
+	if error = json.Unmarshal(bodyRequest, &user); error != nil {
+		log.Fatal(error)
+	}
+
+	db, error := database.Connect()
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	defer db.Close()
+
+	repository := repositories.NewUsersRepository(db)
+
+	userID, error := repository.Create(user)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserted: %d\n", userID)))
 }
 
 func ReadUsers(w http.ResponseWriter, r *http.Request) {
