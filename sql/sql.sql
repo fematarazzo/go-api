be76da0ac8b31a3d91108b1e8631e4feb957dc37
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-26 15:20:13 -0300

    add: sql file to create our database and table

---
+++ sql/sql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+USE devbook;
+
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE usuarios(
+  id int auto_increment primary key,
+  name varchar(50) not null,
+  nickname varchar(50) not null unique,
+  email varchar(50) not null unique,
+  password varchar(50) not null unique,
+  createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
