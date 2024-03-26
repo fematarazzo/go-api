9501ef752ee70c1ef79ffaa0e9cffb40fb43012e
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-26 15:20:25 -0300

    add: config to setup all necessary envs

---
+++ src/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	DBStringConnection = ""
+	Port               = 0
+)
+
+func Load() {
+	var error error
+
+	if error := godotenv.Load(); error != nil {
+		log.Fatal(error)
+	}
+
+	Port, error = strconv.Atoi(os.Getenv("API_PORT"))
+	if error != nil {
+		Port = 9000
+	}
+
+	DBStringConnection = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
