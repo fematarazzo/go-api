ab7dbc68d6f113cb1dffc2f741dd32c9958978ba
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-27 12:22:41 -0300

    add: Validate, Format and Prepare functions add to model

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if error := user.validate(); error != nil {
+		return error
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("Name is mandatory and cannot be empty")
+	}
+
+	if user.Nickname == "" {
+		return errors.New("Nickname is mandatory and cannot be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email is mandatory and cannot be empty")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password is mandatory and cannot be empty")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nickname = strings.TrimSpace(user.Nickname)
+	user.Email = strings.TrimSpace(user.Email)
 }
