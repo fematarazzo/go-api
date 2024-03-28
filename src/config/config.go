241ee7b2a4408ce14c9e3ad6354de34d58ca3ea9
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-28 12:22:47 -0300

    fix: typo

--- src/config/config.go
+++ src/config/config.go
@@ -27,8 +27,8 @@ func Load() {
 		Port = 9000
 	}
 	DBStringConnection = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_USUARIO"),
-		os.Getenv("DB_SENHA"),
-		os.Getenv("DB_NOME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
 	)
 }
