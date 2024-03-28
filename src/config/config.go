f371b0aa89ab4dcab2dd0ff56acbbcc0fd08a0b6
Felipe Matarazzo <felipemps@protonmail.com>
2024-03-28 11:02:55 -0300

    fix: revert to use GoDotEnv package

--- src/config/config.go
+++ src/config/config.go
@@ -1,65 +1,34 @@
 package config
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 var (
 	DBStringConnection = ""
-	DBUser             = ""
-	DBPassword         = ""
-	DBName             = ""
 	Port               = 0
+	SecretKey          []byte
 )
 
-func loadEnvFromFile() error {
-	file, error := os.Open(".env")
-	if error != nil {
-		log.Fatal(error)
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.SplitN(line, "=", 2)
-		switch parts[0] {
-		case "DB_USER":
-			DBUser = parts[1]
-		case "DB_PASSWORD":
-			DBPassword = parts[1]
-		case "DB_NAME":
-			DBName = parts[1]
-		case "API_PORT":
-			Port, error = strconv.Atoi(parts[1])
-			if error != nil {
-				Port = 9000
-			}
-		default:
-			log.Fatal(error)
-		}
-	}
-
-	if error := scanner.Err(); error != nil {
-		return error
-	}
-
-	return nil
-}
-
 func Load() {
-	if error := loadEnvFromFile(); error != nil {
+	var error error
+
+	if error = godotenv.Load(); error != nil {
 		log.Fatal(error)
+	}
+
+	Port, error = strconv.Atoi(os.Getenv("API_PORT"))
+	if error != nil {
+		Port = 9000
 	}
 	DBStringConnection = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
-		DBUser,
-		DBPassword,
-		DBName,
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
 	)
 }
