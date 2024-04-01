e827a8fc1fee79ab66f03457c94f25d40c09e2fe
Felipe Matarazzo <felipemps@protonmail.com>
2024-04-01 18:38:42 -0300

    typo fix

--- main.go
+++ main.go
@@ -12,10 +12,8 @@ import (
 func main() {
 	config.Load()
 
-	fmt.Println(config.DBStringConnection)
-
 	r := router.Generate()
 
-	log.Printf("Listening to port %d", config.Port)
+	log.Printf("Listening to port %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
